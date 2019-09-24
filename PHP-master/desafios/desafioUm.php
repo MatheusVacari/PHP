2ac793d7dc52d4ae8e2f99557994ae6b67bf4a5e
MatheusVacari <42096349+MatheusVacari@users.noreply.github.com>
2019-09-24 20:25:43 -0300

    24-09-19

---
+++ PHP-master/desafios/desafioUm.php
@@ -0,0 +1,9 @@
+<?php
+
+$contador = 1;
+
+while ($contador <= 99){
+    echo "#$contador" . "<br>";
+    $contador = $contado + 2;
+}
+?>
---
+++ PHP-master/desafios/tabuada.php
@@ -0,0 +1,10 @@
+<?php
+
+$numero = 7;
+$controle = 1;
+
+while($controle < 10){
+    echo $numero . " x " . $controle . " = ";
+    echo $numero * $controle . "<br>";
+    $controle++;
+}
